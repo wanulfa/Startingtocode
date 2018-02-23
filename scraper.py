66406402e38370a89bd7c83e74affdc152c4e504
wanulfa <32190655+wanulfa@users.noreply.github.com>
2018-02-23 12:35:33 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
+import scraperwiki
+# library to scrape a webpage
+import lxml.html
+# another library to read it
+
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
+html = scraperwiki.scrape("http://foo.com")
+print html
+
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
